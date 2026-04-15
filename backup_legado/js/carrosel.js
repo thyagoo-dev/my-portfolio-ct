c2226e551722166494f6088eeb36bf11d589e641
Victorkaue333 <kaue.alves.pg@gmail.com>
2026-04-15 09:53:11 -0300

    chore: remove unused configuration files and legacy scripts

--- backup_legado/js/carrosel.js
+++
@@ -1,8 +0,0 @@
-let currentSlide = 0;
-
-function moveSlide(step) {
-    const slides = document.querySelectorAll('.carousel-slide');
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + step + slides.length) % slides.length;
-    slides[currentSlide].classList.add('active');
-}
