bfb1bda6fa648b21fb35bce1274785860667749b
Victorkaue333 <kaue.alves.pg@gmail.com>
2026-05-12 17:14:04 -0300

    feat: implement React root rendering and integrate Devicon via local package installation

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './i18n';
+import 'devicon/devicon.min.css';
 import './styles/variables.css';
 import './styles/reset.css';
 import './styles/global.css';
